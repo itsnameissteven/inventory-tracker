1dba1ef86ef4b8b7819ffca40c505a6bf47c1c01
Steven Mancine <steven.mancine@charter.com>
2025-03-30 11:45:23 -0600

    Fix scroll

--- front-end/app/root.tsx
+++ front-end/app/root.tsx
@@ -48,7 +48,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
     { name: 'Categories', to: '/categories' },
   ];
   return (
-    <html lang="en" className=" dark h-full">
+    <html lang="en" className=" dark">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
