dd52de3def17fcad3211de6c9c51e6a2925dd092
Steven Mancine <steven.mancine@charter.com>
2025-03-31 21:23:59 -0600

    update with auth

---
+++ backend-service/src/main/java/com/service/model/LoginUserDto.java
@@ -0,0 +1,15 @@
+package com.service.model;
+
+public class LoginUserDto {
+    private String username;
+    
+    private String password;
+    
+    // getters and setters here...
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+}
